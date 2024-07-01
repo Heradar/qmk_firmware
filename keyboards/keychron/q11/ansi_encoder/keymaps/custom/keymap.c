c0e2ec81968d4905fb29c5a8a3796c23eb442c1c
Heradar <44797050+Heradar@users.noreply.github.com>
2024-07-01 16:24:19 -0400

    removed gaming layers, moved rgb keys

--- keyboards/keychron/q11/ansi_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/custom/keymap.c
@@ -14,8 +14,6 @@ enum layers{
     MAC_FN,
     WIN_BASE,
     WIN_FN,
-    GAMING_BASE,
-    GAMING_FN,
 };
 
 /*const key_override_t shift_space_override = ko_make_basic(MOD_MASK_SHIFT, KC_SPC, KC_BSPC);*/
@@ -104,38 +102,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_RMOD,  _______,  _______,  _______,  SEND_EMAIL,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,            _______,
         RGB_HUD,   _______,  _______,  _______,  _______,     _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,            _______,            _______,
         RGB_SAD,   _______,            _______,  _______,     _______,  _______,  _______,  NK_TOGG,  _______,  _______,   _______,  _______,            _______,  _______,
-        RGB_SPD,   _______,  _______,  _______,  _______,      _______,                      _______,             _______,  _______,  _______,  _______,  _______,  _______),
+        RGB_SPD,   _______,  _______,  _______,  _______,     _______,                      _______,             _______,  _______,  _______,  _______,  _______,  _______),
 
-
-    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-
-    [GAMING_BASE] = LAYOUT_91_ansi(
-        WIN_LOCK,  KC_ESC,      KC_F1,    KC_F2,    KC_F3,    KC_F4,  KC_F5,   KC_F6,  KC_F7,  KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL,   KC_MPLY,
-        RGB_TOG,   KC_GRV,      KC_1,     KC_2,     KC_3,     KC_4,   KC_5,    KC_6,   KC_7,   KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        RGB_MOD,   KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        RGB_HUI,   _______,     KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-        RGB_SAI,   KC_LSFT,               KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,   KC_UP,
-        RGB_SPI,   MO(GAMING_FN),  KC_LCTL,  KC_LALT,  KC_LWIN,          KC_SPC,                  KC_ENT,            KC_RWIN,  KC_RALT,  KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT),
-
-
-    [GAMING_FN] = LAYOUT_91_ansi(
-        _______,   _______,  WIN_BRID,  WIN_BRIU,  LGUI(KC_TAB),  LGUI(KC_E),  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,   KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,
-        _______,   _______,  _______,   _______,   _______,       _______,     _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,            _______,
-        RGB_RMOD,  _______,  _______,   _______,   SEND_EMAIL,    _______,     _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,            _______,
-        RGB_HUD,   _______,  _______,   _______,   _______,       _______,     _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,            _______,            _______,
-        RGB_SAD,   _______,             _______,   _______,       _______,     _______,  _______,  NK_TOGG,  _______,  _______,   _______,  _______,            _______,  _______,
-        RGB_SPD,   _______,  _______,   _______,   _______,                    _______,                      _______,             _______,  _______,  _______,  _______,  _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE]    = { ENCODER_CCW_CW(KC_BRMD,  KC_BRMU),   ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]      = { ENCODER_CCW_CW(_______,  _______),   ENCODER_CCW_CW(_______, _______) },
-    [WIN_BASE]    = { ENCODER_CCW_CW(WIN_BRID, WIN_BRIU),  ENCODER_CCW_CW(_______, _______) },
-    [WIN_FN]      = { ENCODER_CCW_CW(_______,  _______),   ENCODER_CCW_CW(_______, _______) },
-    [GAMING_BASE] = { ENCODER_CCW_CW(WIN_BRID, WIN_BRIU),  ENCODER_CCW_CW(_______, _______) },
-    [GAMING_FN]   = { ENCODER_CCW_CW(_______,  _______),   ENCODER_CCW_CW(_______, _______) },
+
+    [MAC_BASE] = { ENCODER_CCW_CW(KC_BRMD,  KC_BRMU),   ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MAC_FN]   = { ENCODER_CCW_CW(_______,  _______),   ENCODER_CCW_CW(_______, _______) },
+    [WIN_BASE] = { ENCODER_CCW_CW(WIN_BRID, WIN_BRIU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_FN]   = { ENCODER_CCW_CW(_______,  _______),   ENCODER_CCW_CW(_______, _______) },
 };
 #endif // ENCODER_MAP_ENABLE
 
